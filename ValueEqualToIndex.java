084ffcf70da5f704ea20de1a422a6d44bbb7c736
abishekchiff <dummygodummy2@gmail.com>
2016-08-05 21:38:39 +0530

    Update ValueEqualToIndex.java

--- ValueEqualToIndex.java
+++ ValueEqualToIndex.java
@@ -8,7 +8,6 @@ public class ValueEqualToIndex {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
-		List<Integer> l=new ArrayList<Integer>();
 		String s=sc.nextLine();
 		int count=0;
 		for(int i=0;i<s.length();i++)
